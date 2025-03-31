b61bc44120925960ce9572ec910cd71e2334f6ad
Yuri561 <hazelirvin@gmail.com>
2025-03-31 12:59:06 -0400

    ignore

--- test_connection.py
+++ test_connection.py
@@ -5,14 +5,13 @@ import os
 load_dotenv()
 MONGO_URI = os.getenv("MONGODB_URI")
 try:
-    # Connect to MongoDB
+
     client = MongoClient(MONGO_URI)
     db = client["casaprodb"]
 
-    # Get a list of collections to test the connection
     collections = db.list_collection_names()
     print("✅ Connection Successful!")
     print("Collections:", collections)
 
 except Exception as e:
-    print(f"❌ Error: {e}")
+    print(f"Error: {e}")
