516c00f81db063db5839c390843e0060b7b4a48d
Yuri561 <hazelirvin@gmail.com>
2025-04-19 23:52:28 -0400

    auth 2

--- utils/auth.py
+++ utils/auth.py
@@ -1,27 +1,24 @@
-from functools import wraps
 from flask import request, jsonify
 import jwt
 from utils.config import TOKEN_KEY
 
 def token_required(f):
+    from functools import wraps
+
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = None
+        token = request.cookies.get("access_token")
 
-        if 'Authorization' in request.headers:
-            auth_header = request.headers['Authorization']
-            if auth_header.startswith('Bearer '):
-                token = auth_header.split(" ")[1]
         if not token:
-            return jsonify({"error": "Token is missing!"}), 400
+            return jsonify({"error": "Token is missing"}), 400
 
         try:
-            decoded = jwt.decode(token, TOKEN_KEY, algorithms=["HS256"])
-            current_user_id = decoded['sub']
+            data = jwt.decode(token, TOKEN_KEY, algorithms=["HS256"])
+            current_user_id = data["sub"]
         except jwt.ExpiredSignatureError:
-            return jsonify({"error": "Token has expired!"}), 401
+            return jsonify({"error": "Token has expired"}), 401
         except jwt.InvalidTokenError:
-            return jsonify({"error": "Invalid token!"}), 401
+            return jsonify({"error": "Invalid token"}), 401
 
         return f(current_user_id, *args, **kwargs)
 
