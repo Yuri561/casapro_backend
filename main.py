3359eb82be45b2978ffb9544887350991b56be46
Yuri561 <hazelirvin@gmail.com>
2025-04-28 19:49:20 -0400

    cors

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask import Flask
 from routes.auth_routes import auth_routes
 from flask_cors import CORS
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Correct CORS Configuration (Allow Multiple Origins)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:5173",
-                                         "https://casapro-pink.vercel.app",
-                                         "https://casapro-backend-o0k1.onrender.com"]}}, supports_credentials=True)
-# Register Blueprints
+CORS(app, resources={r"/*": {"origins": [
+    "http://localhost:5173",
+    "https://casapro-pink.vercel.app"
+]}}, supports_credentials=True)
+
 app.register_blueprint(auth_routes)
 
 @app.route("/")
@@ -17,5 +16,4 @@ def home():
     return "Casa Pro Backend is running!"
 
 if __name__ == "__main__":
-
     app.run(host="0.0.0.0", port=5000)
