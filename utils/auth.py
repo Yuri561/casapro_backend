dc2ee13d5b433079d682e3dba8e1120ccc92c43c
Houbenove (Yuri) Pierre-Louis <121592752+Yuri561@users.noreply.github.com>
2025-04-28 20:43:57 -0400

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -4,22 +4,23 @@ from utils.config import TOKEN_KEY
 from functools import wraps
 
 def token_required(f):
-
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.cookies.get("access_token")
-        if 'access_token' in request.cookies:
-            token = request.cookies.get('access_token')
+        token = None
+        if "Authorization" in request.headers:
+            auth_header = request.headers["Authorization"]
+            token = auth_header.split(" ")[1] if " " in auth_header else None
+
         if not token:
             return jsonify({"error": "Token is missing"}), 401
+
         try:
             data = jwt.decode(token, TOKEN_KEY, algorithms=["HS256"])
-            # print("[DEBUG] JWT Payload:", data)
             current_user_id = data.get("sub")
         except jwt.ExpiredSignatureError:
             return jsonify({"error": "Token has expired"}), 401
         except jwt.InvalidTokenError:
             return jsonify({"error": "Invalid token"}), 401
-        return f(current_user_id, *args, **kwargs)
 
+        return f(current_user_id, *args, **kwargs)
     return decorated
